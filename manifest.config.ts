7dbfe4c1253b364ba37fa7a1cea691b4aa2b1e50
pinowine <z.chen02202510@arts.ac.uk>
2026-07-30 11:25:57 +0800

    upd

--- manifest.config.ts
+++ manifest.config.ts
@@ -30,13 +30,6 @@ export default defineManifest({
     service_worker: "src/background/index.ts",
     type: "module",
   },
-  content_scripts: [
-    {
-      matches: ["<all_urls>"],
-      js: ["src/content/index.ts"],
-      run_at: "document_idle",
-    },
-  ],
   side_panel: {
     default_path: "index.html",
   },
